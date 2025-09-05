9ef9c2d7b3038e16958845e3b1d2a2c0c42649d6
Douglas Bezerra <ddouglasbezerra7@gmail.com>
2025-09-05 18:34:17 -0300

    Adição da seção formulário e adição do componente formulário

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -1,14 +1,33 @@
 import './App.css'
 
 
+
 function Formulario() {
 
     return (
         <>
+
             <section id='formulario'>
-                
-                
-               
+                <h1>Fale Comigo: </h1>
+
+                <form action="">
+
+                    <label htmlFor="">Nome: </label>
+                    <input type="text" id='nome' />
+
+                    <label htmlFor="">Idade: </label>
+                    <input type="number" id='idade' />
+
+                    <label htmlFor="">Telefone: </label>
+                    <input type="tel" id='telefone' />
+
+                    <label htmlFor="">Assunto: </label>
+                    <textarea id="assunto" name="mensagem" rows="5" cols="40"></textarea>
+
+                    <button type="submit">Enviar</button>
+
+                </form>
+
             </section>
         </>
     )
