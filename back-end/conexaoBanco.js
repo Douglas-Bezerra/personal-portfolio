6af452c676e0a3dfd91910910b284b204e5547f3
Douglas Bezerra <ddouglasbezerra7@gmail.com>
2025-09-18 18:40:31 -0300

    Conexão com o banco de dados MongoDB funcionando, adição de arquivo para variáveis de ambiente sensíveis, instalação do pacote mongodb e dotenv, banco está criando documentos corretamente

--- back-end/conexaoBanco.js
+++ back-end/conexaoBanco.js
@@ -1,11 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://dbDouglas:dbDouglas@cluster.1i6ypbc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require('dotenv').config();
 
-let cluster = ''
-let dbName = ''
-let collectionName = ''
-let username = ''
-let password = ''
+
+let cluster = "Cluster"
+let dbName = "personalPortfolio"
+let collectionName = "faleComigo"
+let username = process.env.mongoDB_username
+let password = process.env.mongoDB_password
+
+const uri = `mongodb+srv://${username}:${password}@cluster.1i6ypbc.mongodb.net/?retryWrites=true&w=majority&appName=${cluster}`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -17,15 +21,23 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
   try {
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log("Conectado ao MongoDB 🟢");
+
+    const db = client.db(dbName); // pega o banco
+    const collection = db.collection(collectionName); //pega a coleção
+
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
+
 run().catch(console.dir);
